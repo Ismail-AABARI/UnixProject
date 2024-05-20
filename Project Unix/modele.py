7f392a327166c0303671b2a824a3ff44a8c16859
ismail <i.aabari@esisa.ac.ma>
2024-05-20 15:55:49 +0100

    this is a adding some models

--- Project Unix/modele.py
+++ Project Unix/modele.py
@@ -4,3 +4,33 @@ from dataclasses import dataclass, field
 class Account :
     email:str
     password:str
+
+@dataclass
+class SecureLogs:
+    source_name: str
+    day: str
+    nombre: int
+    time: str
+    localisation: str
+    ssh_protocole: str
+    log_info: str
+    address: str
+
+@dataclass
+class ApacheLogs:
+    source_name: str
+    ip_address: str
+    date: str
+    day: int
+    month: int
+    year: int
+    time: str
+    method: str
+    http: str
+    status: int
+    size: int
+    urls: str
+    software: str
+    operating_system: str
+    browser: str
+    http_request: str
